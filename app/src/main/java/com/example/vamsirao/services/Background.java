5ef92a8e24f3399e10459265c7d36ecf181acf45
Vamsi <s.vamsirao@gmail.com>
2017-01-21 18:46:03 +0530

    Add Service Class

---
+++ app/src/main/java/com/example/vamsirao/services/Background.java
@@ -0,0 +1,33 @@
+package com.example.vamsirao.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by Vamsi Rao on 1/21/2017.
+ */
+
+public class Background extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+       Toast.makeText(this,"Service Started",Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Service is Destroyed",Toast.LENGTH_LONG).show();
+
+    }
+}
