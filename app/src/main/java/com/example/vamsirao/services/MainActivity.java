f67d2cb98c02910ca48e4140588086af4828d9a4
Vamsi <s.vamsirao@gmail.com>
2017-01-21 22:17:19 +0530

    Change Manifest, MainActivity.java and  activity_main.xml

--- app/src/main/java/com/example/vamsirao/services/MainActivity.java
+++ app/src/main/java/com/example/vamsirao/services/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.vamsirao.services;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+   // Button startButton= (Button)findViewById(R.id.start_button);
+
+    public void startService(View view){
+        startService(new Intent(getBaseContext(),Background.class));
+    }
+
+    public void stopService(View view){
+        stopService(new Intent(MainActivity.this,Background.class));
+    }
+
 }
