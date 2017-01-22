96fa32c7dcb07fc32664a05cf7aa26035d2a2a12
Vamsi <s.vamsirao@gmail.com>
2017-01-22 23:16:14 +0530

    Make Pop-up without title bar and blank black activity

--- app/src/main/java/com/example/vamsirao/services/Popup.java
+++ app/src/main/java/com/example/vamsirao/services/Popup.java
@@ -1,13 +1,18 @@
 package com.example.vamsirao.services;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class Popup extends AppCompatActivity {
+public class Popup extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_popup);
     }
 }
