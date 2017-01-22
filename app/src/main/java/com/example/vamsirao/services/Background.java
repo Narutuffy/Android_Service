f4eae688e6dfc99787e01ec0a04efdbcc9a32540
Vamsi <s.vamsirao@gmail.com>
2017-01-22 06:03:42 +0530

    Pop the Pop-up Activity with 60s time interval

--- app/src/main/java/com/example/vamsirao/services/Background.java
+++ app/src/main/java/com/example/vamsirao/services/Background.java
@@ -1,16 +1,23 @@
 package com.example.vamsirao.services;
 
 import android.app.Service;
+
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Vamsi Rao on 1/21/2017.
  */
 
 public class Background extends Service {
+
+    private static Timer timer = new Timer();
+    Intent intent= new Intent();
 
     @Nullable
     @Override
@@ -20,14 +27,27 @@ public class Background extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        timer.scheduleAtFixedRate(new mainTask(),0,60000);
        Toast.makeText(this,"Service Started",Toast.LENGTH_LONG).show();
         return START_STICKY;
     }
+
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Toast.makeText(this,"Service is Destroyed",Toast.LENGTH_LONG).show();
 
+    }
+
+    private class mainTask extends TimerTask{
+
+        @Override
+        public void run() {
+
+            intent.setClass(Background.this,Popup.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 }
