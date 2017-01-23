524e595aabdff252b6b1e3a87e87f81731c11bf6
Vamsi <s.vamsirao@gmail.com>
2017-01-23 23:10:50 +0530

    Set a Toast Message after 10 secs using Alarmmanager

--- app/src/main/java/com/example/vamsirao/services/MainActivity.java
+++ app/src/main/java/com/example/vamsirao/services/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.vamsirao.services;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -16,14 +19,21 @@ public class MainActivity extends Activity {
     }
 
 
-   // Button startButton= (Button)findViewById(R.id.start_button);
 
     public void startService(View view){
-        startService(new Intent(getBaseContext(),Background.class));
+//        startService(new Intent(getBaseContext(),Background.class));
+        Intent intent= new Intent(MainActivity.this,Broadcast.class);
+        //performing a broadcast with pnding intent
+        PendingIntent pendingIntent= PendingIntent.getBroadcast(
+                                        this.getApplicationContext(),7777,intent,0);
+
+        AlarmManager alarmManager= (AlarmManager)getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+10000,pendingIntent);
+        Toast.makeText(this,"Alarm set in 10 secs",Toast.LENGTH_SHORT).show();
     }
 
-    public void stopService(View view){
+  /*  public void stopService(View view){
         stopService(new Intent(MainActivity.this,Background.class));
     }
-
+*/
 }
