7488753926a1873ac925b3753b9d51ff1ac31cbd
Vamsi <s.vamsirao@gmail.com>
2017-01-24 23:33:21 +0530

    Pop up (1min,1min) (time period,interval) with an ad

--- app/src/main/java/com/example/vamsirao/services/Popup.java
+++ app/src/main/java/com/example/vamsirao/services/Popup.java
@@ -1,11 +1,14 @@
 package com.example.vamsirao.services;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Popup extends Activity {
 
@@ -16,12 +19,25 @@ public class Popup extends Activity {
 
         setContentView(R.layout.activity_popup);
 
+        VideoView videoView=(VideoView)findViewById(R.id.video_view);
+        //Creating MediaController
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        //specify the location of media file
+        Uri uri= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.coke_ad);
+        //Setting Mediacontroller and URI, then starting the Video View
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+
         Handler handler= new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 finish();
             }
-        },5000);
+        },60*1000);
     }
 }
