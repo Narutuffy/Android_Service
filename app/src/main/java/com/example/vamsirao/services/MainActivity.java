1db3685e120209af0dc2820479103538ad3bdc6a
Vamsi <s.vamsirao@gmail.com>
2017-01-25 19:45:52 +0530

    Stop the Pop-ups when the Stop button is pressed

--- app/src/main/java/com/example/vamsirao/services/MainActivity.java
+++ app/src/main/java/com/example/vamsirao/services/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.vamsirao.services;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,17 +29,24 @@ public class MainActivity extends Activity {
         //performing a broadcast with PendingIntent
         PendingIntent pendingIntent= PendingIntent.getBroadcast(
                                         this.getApplicationContext(),0,intent,0);
-        long interval= 2*60*1000;
+        long interval= 1*60*1000;
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.SECOND, 20);
+        calendar.add(Calendar.SECOND, 60);
         AlarmManager alarmManager= (AlarmManager)getSystemService(ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),interval,pendingIntent);
-        Toast.makeText(this,"Alarm set in 1min",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Pop-up set in 1min",Toast.LENGTH_SHORT).show();
     }
 
-  /*  public void stopService(View view){
-        stopService(new Intent(MainActivity.this,Background.class));
+    public void stopService(View view){
+        //Creating exactly the same pending intent which was used to broadcast, to cancel
+        Intent intent= new Intent(MainActivity.this,Broadcast.class);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 0, intent, 0);
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this,"Pop-ups cancelled",Toast.LENGTH_SHORT).show();
+
+
     }
-*/
+
 }
